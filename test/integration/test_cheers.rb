7a390fe87e294d261d9348e62e61292d896a29cc
Margaret Clinard <margaret.clinard@gmail.com>
2015-05-05 10:26:09 -0500

    Basic structure with 3 failing tests to assert rake works.

---
+++ test/integration/test_cheers.rb
@@ -0,0 +1,7 @@
+require_relative '../test_helper'
+
+class TestCheers < Minitest::Test
+  def test_something
+    assert false
+  end
+end
---
+++ test/units/test_birthday_parser.rb
@@ -0,0 +1,7 @@
+require_relative '../test_helper'
+
+class TestBirthdayParser < Minitest::Test
+  def test_something
+    assert false
+  end
+end
