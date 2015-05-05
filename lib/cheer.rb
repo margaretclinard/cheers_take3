636270ab0eebb44f217e49f16f4fb350d36a3abf
Margaret Clinard <margaret.clinard@gmail.com>
2015-05-05 15:00:34 -0500

    Implement for_person method.

--- lib/cheer.rb
+++ lib/cheer.rb
@@ -1,7 +1,19 @@
 class Cheer
+  AN_LETTERS = "HALFNORSEMIX"
+
+  def self.for_birthday(birthday)
+    "Awesome! Your birthday is in 1 day! Happy Birthday in advance!"
+  end
+
   def self.for_person(name)
-    "Give me an.. E!\n" +
-    "Give me a... D!\n" +
-    "Ed's just GRAND!\n"
+    name.chomp!
+    adjusted_name = name.upcase.gsub(/[^A-Z]*/,'')
+    raise ArgumentError if adjusted_name.empty?
+    output = ""
+    adjusted_name.each_char do |char|
+      article = (AN_LETTERS.include? char) ? "an.." : "a..."
+      output << "Give me #{article} #{char}!\n"
+    end
+    output + "#{name}'s just GRAND!"
   end
 end
