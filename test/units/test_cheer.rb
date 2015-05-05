7a1252dcd9adc86902b68cd20dd28187e3d1ae54
Margaret Clinard <margaret.clinard@gmail.com>
2015-05-05 11:55:15 -0500

    Updated test_cheer to match class name of Cheer. Added test on for_person method.

--- test/units/test_cheer.rb
+++ test/units/test_cheer.rb
@@ -1,7 +1,32 @@
 require_relative '../test_helper'
+require_relative '../../lib/cheer'
 
-class TestCheerGenerator < Minitest::Test
-  def test_something
-    assert false
+class TestCheer < Minitest::Test
+  def test_valid_name
+    actual = Cheer.for_person("Ed")
+    expected = "Give me an.. E!\n" +
+               "Give me a... D!\n" +
+               "Ed's just GRAND!\n"
+    assert_equal expected, actual
+  end
+
+  def test_name_with_spaces
+    fail
+  end
+
+  def test_name_with_hypens
+    fail
+  end
+
+  def test_name_thats_blank
+    fail
+  end
+
+  def test_name_thats_whitespace
+    fail
+  end
+
+  def test_name_that_has_no_word_characters
+    fail
   end
 end
